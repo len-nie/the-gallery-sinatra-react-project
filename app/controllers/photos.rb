793aef639a3b52ddcfe5150c0c246bbbb1494866
mbotela <leemaliy53@gmail.com>
2023-08-02 06:04:27 +0300

    remove description for images

--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -7,15 +7,15 @@ class PhotosApp
     response = Rack::Response.new
 
     photos = [
-      { id: 1, title: 'Photo 1', source: '/images/photo1.jpg', description: 'Description for Photo 1' },
-      { id: 2, title: 'Photo 2', source: '/images/photo2.jpg', description: 'Description for Photo 2' },
-      { id: 3, title: 'Photo 3', source: '/images/photo3.jpg', description: 'Description for Photo 3' },
-      { id: 4, title: 'Photo 4', source: '/images/photo4.jpg', description: 'Description for Photo 4' },
-      { id: 5, title: 'Photo 5', source: '/images/photo5.jpg', description: 'Description for Photo 5' },
-      { id: 6, title: 'Photo 6', source: '/images/photo6.jpg', description: 'Description for Photo 6' },
-      { id: 7, title: 'Photo 7', source: '/images/photo7.jpg', description: 'Description for Photo 7' },
-      { id: 8, title: 'Photo 8', source: '/images/photo8.jpg', description: 'Description for Photo 8' },
-      { id: 9, title: 'Photo 9', source: '/images/photo9.jpg', description: 'Description for Photo 9' }
+      { id: 1, title: 'Photo 1', source: '/images/photo1.jpg', },
+      { id: 2, title: 'Photo 2', source: '/images/photo2.jpg', },
+      { id: 3, title: 'Photo 3', source: '/images/photo3.jpg', },
+      { id: 4, title: 'Photo 4', source: '/images/photo4.jpg', },
+      { id: 5, title: 'Photo 5', source: '/images/photo5.jpg', },
+      { id: 6, title: 'Photo 6', source: '/images/photo6.jpg', },
+      { id: 7, title: 'Photo 7', source: '/images/photo7.jpg', },
+      { id: 8, title: 'Photo 8', source: '/images/photo8.jpg', },
+      { id: 9, title: 'Photo 9', source: '/images/photo9.jpg', }
     ]
 
     if request.path_info == '/photos' && request.get?
