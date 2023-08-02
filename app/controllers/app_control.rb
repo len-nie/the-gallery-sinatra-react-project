6bba65a1ece81d963f15b0fb3faeb4cd38a6336c
mbotela <leemaliy53@gmail.com>
2023-08-02 19:30:04 +0300

    added necessary "require" logic

---
+++ app/controllers/app_control.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './models/photo'
+require 'json'
+
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json' 
+  
+  # Add your routes here
+
+
+ 
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get "/" do
+    { message: "Good luck with your project!" }.to_json
+  end
+
+end
