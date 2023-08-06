97aaabd24923802858b0422a76b277de8c1cfd0e
mbotela <leemaliy53@gmail.com>
2023-08-07 01:05:04 +0300

    add images to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
+puts "clearing data"
+Photos.destroy_all
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+
+photos = [
+    { image: "app/images/photo1.jpg", purchase: false },
+    { image: "app/images/photo2.jpg", purchase: false },
+    { image: "app/images/photo3.jpg", purchase: false },
+    { image: "app/images/photo4.jpg", purchase: false },
+    { image: "app/images/photo5.jpg", purchase: false },
+    { image: "app/images/photo6.jpg", purchase: false },
+    { image: "app/images/photo7.jpg", purchase: false },
+    { image: "app/images/photo8.jpg", purchase: false },
+    { image: "app/images/photo9.jpg", purchase: false },
+];
+
+Photos.create!(photos)
+
+
 
 puts "✅ Done seeding!"
