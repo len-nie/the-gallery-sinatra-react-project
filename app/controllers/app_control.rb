b07dac41ef0366125b318a29c27fae1f5d087383
mbotela <leemaliy53@gmail.com>
2023-08-07 01:05:20 +0300

    correct route addition

--- app/controllers/app_control.rb
+++ app/controllers/app_control.rb
@@ -1,13 +1,24 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require './models/photo'
+# require './models/photo'
 require 'json'
 
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json' 
   
   # Add your routes here
+  get "/photos" do 
+    Photos.all.to_json
+  end
 
+patch "/photos/:id" do 
+  photo = Photos.find(params[:id])
+  if photo.update(params)
+    { message: "Good luck with your project!" }.to_json
+  else 
+    { message: "error." }.to_json
+  end
+end
 
  
   configure do
